72dfd21fa62f0077c74bbf800e7725583b1599cd
durveshramane26 <durveshramane6@gmail.com>
2022-07-27 16:52:06 +0530

    fix: minor fixes

--- Drinking-water/script.js
+++ Drinking-water/script.js
@@ -12,12 +12,12 @@ smallCups.forEach((cup, idx) => {
 function highlightCups(idx) {
     if (idx===7 && smallCups[idx].classList.contains("full")) idx--;
     else if(smallCups[idx].classList.contains('full') && ! smallCups[idx].nextElementSibling.classList.contains('full')) {
-        idx--;
+        idx--
     }
 
     smallCups.forEach((cup, idx2) => {
         if (idx2 <= idx) {
-            cup.classList.add('full');
+            cup.classList.add('full')
         }
         else {
             cup.classList.remove('full')
